1f442fdf7d57601bdddb513cd36732952b7decdc
Kollaider <kolja87@tut.by>
2023-02-24 15:57:19 +0300

    1. added Post, Comment, Category, Tag models; 2. set pagination; 3. templates refactoring.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+class Post(models.Model):
+    """Post model"""
+    title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    image = models.ImageField(upload_to='blog_images/', blank=True)
+    categories = models.ManyToManyField('Category')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """Comment model"""
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.text[:20] +  '...'
+
+
+class Category(models.Model):
+    """Category model"""
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    """Category model"""
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
