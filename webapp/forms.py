4d221fa4452a30d3ed4f38aed164aca1294eb166
Kollaider <kolja87@tut.by>
2023-03-02 16:42:41 +0300

    1. new templates: login, logout, register_done and register_user; 2. transfer to CBV; 3. context_processors for user.

---
+++ webapp/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth import password_validation
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+class RegisterUserForm(forms.ModelForm):
+    email = forms.EmailField(required=True, label='Адрес электронной почты')
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput,
+                                help_text=password_validation.password_validators_help_text_html())
+    password2 = forms.CharField(label='Password (again)',
+                                widget=forms.PasswordInput,
+                                help_text='Введите тот же самый пароль еще раз для проверки')
+
+    def clean_password1(self):
+        password1 = self.cleaned_data['password1']
+        if password1:
+            password_validation.validate_password(password1)
+        return password1
+
+    def clean(self):
+        super().clean()
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 and password2 and password1 != password2:
+            errors = {'password2': ValidationError(
+                'The entered passwords do not match', code='password_mismatch')}
+            raise ValidationError(errors)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.is_active = True
+        user.is_activated = True
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2',
+                  'first_name', 'last_name')
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
+
+from webapp.forms import RegisterUserForm
 from webapp.models import Car
 
 
@@ -13,23 +20,57 @@ def index(request):
     return render(request, 'webapp/index.html', context=context)
 
 
-def about(request):
-    return render(request, 'webapp/about.html')
+class AboutView(TemplateView):
+    template_name = 'webapp/about.html'
 
 
-def services(request):
-    return render(request, 'webapp/services.html')
+class ServicesView(TemplateView):
+    template_name = 'webapp/services.html'
 
 
-def contact(request):
-    return render(request, 'webapp/contact.html')
+class ContactView(LoginRequiredMixin, TemplateView):
+    template_name = 'webapp/contact.html'
 
 
-def cars(request):
-    """View for getting paginated cars."""
-    cars = Car.objects.all()
-    paginator = Paginator(cars, 4)
-    page_number = request.GET.get('page')
-    page_objs = paginator.get_page(page_number)
-    context = {'page_objs': page_objs}
-    return render(request, 'webapp/cars.html', context=context)
+class CarListView(ListView):
+    """Car List View for getting list objects"""
+    template_name = 'webapp/cars.html'
+    context_object_name = 'cars'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        page_number = self.request.GET.get('page')
+        paginator = Paginator(context['object_list'], 4)
+        page_objs = paginator.get_page(page_number)
+        context['page_objs'] = page_objs
+        return super().get_context_data(**context)
+
+
+    def get_queryset(self):
+        return Car.objects.all()
+
+
+class CarDetailView(DetailView):
+    """Car List View for getting detail object"""
+    model = Car
+    template_name = 'webapp/car_detail.html'
+
+
+class CRLoginView(LoginView):
+    template_name = 'webapp/login.html'
+    redirect_authenticated_user = True
+
+
+class CRLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'webapp/logout.html'
+
+
+class RegisterUserView(CreateView):
+    model = User
+    template_name = 'webapp/register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('webapp:register_done')
+
+
+class RegisterDoneView(TemplateView):
+    template_name = 'webapp/register_done.html'
